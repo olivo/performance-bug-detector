ea6c72c96d466879950759ae1fac532715dd7331
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-03-17 23:19:54 -0500

    Added compilation and execution scripts, and dummy analysis class

---
+++ src/PerformanceBugDetector.java
@@ -0,0 +1,12 @@
+import soot.*;
+import soot.jimple.*;
+import soot.util.*;
+
+public class PerformanceBugDetector {
+
+    public static void main(String[] args) {
+
+	soot.Main.main(args);
+	System.out.println("Finished analysis.");
+    }
+}
