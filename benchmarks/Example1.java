36dc7f32b49d9e6d0b0bd243ed89c5548ad779d1
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-03-17 22:11:56 -0500

    Added example of performance bug

---
+++ benchmarks/Example1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Example1 {
+
+
+    public static void main(String[] args) {
+
+	ArrayList<Integer> list = new ArrayList<>();
+	int N = 10;
+
+	for(int i = 0; i < N; i++) {
+	    list.add(new Integer(i));
+	}
+
+	int min = Integer.MAX_VALUE;
+	for(Integer Int : list) {
+	    for(Integer IntVal : list) {
+		if(IntVal.intValue() < min) {
+		    min = IntVal.intValue();
+		}
+	    }
+	}
+    }
+}
