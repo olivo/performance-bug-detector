756d623c282b976dc29c0c90aa3a12dca10d4c8f
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-03-20 19:51:45 -0500

    Added dummy class for performance bug analysis

--- src/PerformanceBugDetector.java
+++ src/PerformanceBugDetector.java
@@ -5,8 +5,11 @@ import soot.util.*;
 public class PerformanceBugDetector {
 
     public static void main(String[] args) {
-
+	System.out.println("=== STARTED PERFORMANCE BUG DETECTION ===");
+	Pack pack = PackManager.v().getPack("wjtp");
+	Transform transform = new Transform("wjtp.PerformanceBugAnalysis", PerformanceBugAnalysis.v());
+	pack.add(transform);
 	soot.Main.main(args);
-	System.out.println("Finished analysis.");
+	System.out.println("=== FINISHED PERFORMANCE BUG DETECTION ===");
     }
 }
