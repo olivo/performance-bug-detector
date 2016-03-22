0d3e36abae502bc82a767b313a59d3290e3f8d92
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-03-21 23:55:52 -0500

    Added call graph construction, and extracting of methods in benchmark example class

--- src/PerformanceBugAnalysis.java
+++ src/PerformanceBugAnalysis.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import soot.*;
 import soot.jimple.*;
+import soot.jimple.toolkits.callgraph.CallGraph;
 import soot.util.*;
 
 class PerformanceBugAnalysis extends SceneTransformer {
@@ -9,6 +10,12 @@ class PerformanceBugAnalysis extends SceneTransformer {
     protected void internalTransform(String phaseName, Map options) {
 
 	System.out.println("=== STARTED PERFORMANCE BUG ANALYSIS ===");
+
+	CallGraph cg = Scene.v().getCallGraph();
+	SootClass sootClass = Scene.v().getSootClass("Example1");
+	for(SootMethod sootMethod : sootClass.getMethods()) {
+	    System.out.println("Found method: " + sootMethod.getSignature());
+	}
 
 	System.out.println("=== FINISHED PERFORMANCE BUG ANALYSIS ===");
     }
