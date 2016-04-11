513b445ec3725db328c2d135f233bf19da4affbd
Oswaldo Olivo <olivo@cs.utexas.edu>
2016-04-11 18:07:15 -0500

    Added initial loop detection

--- src/SummaryAnalysis.java
+++ src/SummaryAnalysis.java
@@ -1,7 +1,9 @@
 import soot.*;
 import soot.jimple.*;
+import soot.jimple.toolkits.annotation.logic.Loop;
 import soot.jimple.toolkits.callgraph.CallGraph;
 import soot.jimple.toolkits.callgraph.Edge;
+import soot.toolkits.graph.LoopNestTree;
 import soot.toolkits.graph.pdg.EnhancedUnitGraph;
 import soot.toolkits.graph.UnitGraph;
 import soot.util.*;
@@ -34,8 +36,23 @@ class SummaryAnalysis {
     }
 
     public boolean computeMethodSummary(SootMethod method, GlobalSummary globalSummary){
+	System.out.println("Analyzing method: " + method.getDeclaration());
 	GlobalSummaryKey key = new GlobalSummaryKey(method.getDeclaration());
 	Summary summary = new Summary();
+
+	if(method.hasActiveBody()){
+	    Body methodBody = method.getActiveBody();
+	    LoopNestTree loop_tree=new LoopNestTree(methodBody);
+
+	    Iterator loopIt=loop_tree.iterator();
+	    while(loopIt.hasNext()) {
+		Loop loop=(Loop)loopIt.next();
+		System.out.println("Found loop.");
+	    }
+	}else{
+	    System.out.println("Skipping " + method.getDeclaration() + ": No active body.");
+	}
+
 	globalSummary.put(key, summary);
 	return true;
     }
